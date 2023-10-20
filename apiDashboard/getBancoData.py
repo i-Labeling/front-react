16dbc62f1bd9ce8200e4a913ec5ecfc6bb6bb860
Walfredo-Benevendes13 <129102915+Walfredo-Benevendes13@users.noreply.github.com>
2023-10-20 14:03:15 -0400

    Add files via upload

---
+++ apiDashboard/getBancoData.py
@@ -0,0 +1,279 @@
+from flask import Flask, jsonify, request,Response
+import psycopg2
+from psycopg2 import extras
+import json
+from flask_cors import CORS
+import time
+import os
+import sys
+sys.path.append("C:/Users/walfr/OneDrive/Ambiente de Trabalho/iLabeling_conexao/new_front_react/front-react/apiDashboard/") 
+global url
+url = "C:/Users/walfr/OneDrive/Ambiente de Trabalho/iLabeling_conexao/new_front_react/front-react/apiDashboard/"
+
+app = Flask(__name__)
+CORS(app)
+connection = psycopg2.connect(
+        dbname="bd_ilabeling",
+        user="postgres",
+        password="2023",
+        host="localhost",
+        port="5432"
+    )
+# Função para buscar os dados mais recentes da tabela info_arq
+def get_latest_data_from_postgres():
+   # try:
+    # Conecta-se ao banco de dados PostgreSQL
+    
+    cur = connection.cursor()
+
+    # Executa a consulta SQL para selecionar os dados mais recentes da tabela "info_arq"
+    cur.execute("SELECT * FROM info_arq ORDER BY data_insercao DESC LIMIT 1")
+    resultado = list(cur.fetchone())#.fetchone()
+    # Fecha a conexão com o banco de dados
+    
+    
+
+    if resultado:
+        # Crie um dicionário com as chaves correspondentes às colunas e atribua os valores        
+        data_dict = {
+            "id": str(resultado[0]),
+            "customer": str(resultado[1]),
+            "tray": str(resultado[2]),
+            "totalCycleTime": float(str(resultado[3]).replace("Decimal('')","")),
+            "minutesPerTray": str(resultado[4]),
+            "timePerMemory": float(str(resultado[5]).replace("Decimal('')","")),
+            "creamBelowA": int(resultado[6]),
+            "indexMemoryError": str(resultado[7]).replace("["," ").replace("]"," "),
+            "inspectionErrors": str(resultado[8]).replace("["," ").replace("]"," "),
+            "cameraError": int(resultado[9]),
+            "positionAndError": str(resultado[10]).replace("["," ").replace("]"," "),
+            "order": str(resultado[11])
+                # Formato ISO da data
+            
+            # 'tray':"0",
+            # 'totalCycleTime': "0",
+            # 'minutesPerTray': "0",
+            # 'timePerMemory': "0",
+            # 'creamBelowA':"0",
+            # 'inspectionErrors': "0",
+            # 'cameraError': "0",
+            # 'indexMemoryError': "0",
+            # 'positionAndError': "0",
+            #'order': "primeira ordem"
+        }
+        return data_dict
+    else:
+        
+        return {}
+
+    #except Exception as e:
+     #   return {"erro": str(e)}
+
+@app.route('/', methods=['GET'])
+def index():
+    # Obtém os dados mais recentes do PostgreSQL em forma de dicionário
+    data = get_latest_data_from_postgres()
+    return data  # Retorna os dados como um JSON
+
+@app.route('/post', methods=['POST'])
+def post_example():
+    if request.method == 'POST':
+        datas = request.json  # Suponha que os dados sejam enviados como JSON
+        # Verifique se o arquivo JSON já existe
+        if not os.path.exists('dados.json'):
+            # Se não existir, crie um arquivo vazio
+            with open(url+'dados.json', 'w') as arquivo_json:
+                json.dump({}, arquivo_json)  # Cria um objeto JSON vazio
+        #print("to aqui")
+        # Abra o arquivo JSON existente
+        with open(url+'dados.json', 'r') as arquivo_json:
+            dados_existentes = json.load(arquivo_json)
+            #print(dados_existentes)
+
+        # Escreva os dados atualizados de volta para o arquivo JSON
+        with open(url+'dados.json', 'w') as arquivo_json:
+            #print("to aqui 2")
+            json.dump(datas, arquivo_json)
+            print(datas)
+       # att(datas)
+        resultado = {"mensagem": "Dados recebidos com sucesso", "dados": datas}
+        return resultado, 200  # 200 indica sucesso
+    else:
+        return "Método não permitido", 405 
+    
+@app.route('/costumer', methods = ['GET'])
+def get_costumer():
+    data = [{
+            "name": "FLEXTRONICS - IPG<",
+            },
+            {
+            "name": "WINTRONIC",
+            },
+            {
+            "name": "AMD",
+            },
+            {
+             "name":"SAE-South America Eletronics"
+            }
+            ]
+    return data    
+
+@app.route('/sse/statusDevice')
+def status_device():
+    def event_stream():
+        while True:
+            with open(url+'dataDevice.json', 'r') as file:
+                new_data = json.load(file)
+            yield f"data:{json.dumps(new_data)}\n\n"
+            time.sleep(1)
+    return Response(event_stream(), content_type='text/event-stream')
+
+@app.route('/sse/statusProcess')
+def status_process():
+    def event_stream():
+        while True:
+            with open(url+'dataProcess.json', 'r') as file:
+                new_data = json.load(file)
+            yield f"data:{json.dumps(new_data)}\n\n"
+            time.sleep(1)
+    return Response(event_stream(), content_type='text/event-stream')
+
+@app.route('/sse/log')
+def log():
+    def event_stream():
+        while True:
+            with open(url+'log.json', 'r') as file:
+                new_data = json.load(file)
+            yield f"data:{json.dumps(new_data)}\n\n"
+            time.sleep(1)
+    return Response(event_stream(), content_type='text/event-stream')
+
+
+
+
+@app.route('/dashboard/graph1', methods=['GET'])
+def dash_hourxunits():
+    costumer = request.args.getlist('costumer')
+    date = request.args.get('date')
+    typeM = request.args.getlist('typeM')
+    dictSelect = {
+        'costumer': costumer,
+        'date' : date,
+        'typeM' : typeM,
+    }
+    try:   
+        connection = psycopg2.connect(
+        dbname="bd_ilabeling",
+        user="postgres",
+        password="2023",
+        host="localhost",
+        port="5432"
+    )
+        cursor = connection.cursor(cursor_factory=extras.DictCursor)
+        
+        
+        cursor.execute("SELECT SUM(CAST(quant_memory AS INTEGER)) AS quantity, CAST(EXTRACT(HOUR FROM data_insercao) AS VARCHAR) AS hour, SUM(CAST(quant_memory AS INTEGER)) FILTER (WHERE type_memory = 'sodimm') AS sodimm,SUM(CAST(quant_memory AS INTEGER)) FILTER (WHERE type_memory = 'udimm') AS udimm FROM info_arq WHERE data_insercao::DATE = %s AND customer =  ANY(%s) AND type_memory = ANY(%s) GROUP BY EXTRACT(HOUR FROM data_insercao),customer",(dictSelect['date'], dictSelect['costumer'], dictSelect['typeM'],))
+        result = cursor.fetchall()
+
+        column_names = [desc[0] for desc in cursor.description]
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+        return jsonify(result_dicts)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+#Rota do grafico unidade x day
+@app.route('/dashboard/graph2', methods=['GET'])
+def dash_dayxunits():
+    costumer = request.args.getlist('costumer')
+    date = request.args.get('date')
+    typeM = request.args.getlist('typeM')
+    dictSelect = {
+        'costumer': costumer,
+        'date' : date,
+        'typeM' : typeM,
+    }
+    try:        
+        cursor = connection.cursor(cursor_factory=extras.DictCursor)
+
+        # EXTRACT(EPOCH FROM coluna_timestamp) = seu_valor_inteiro
+        # cursor.execute("SELECT CAST(EXTRACT(HOUR FROM data_insercao) AS VARCHAR)  AS hr, COUNT(DISTINCT CASE WHEN type_memory = 'sodimm' THEN quant_memory END) AS count_distinct_SODIMM, COUNT(DISTINCT CASE WHEN type_memory = 'udimm' THEN quant_memory END) AS count_distinct_UDIMM FROM info_arq WHERE data_insercao::DATE = %s AND customer =  ANY(%s) and type_memory = ANY(%s) GROUP BY EXTRACT(HOUR FROM data_insercao)",(dictSelect['date'],dictSelect['costumer'],dictSelect['typeM'],))
+        # 
+        cursor.execute("SELECT SUM(CAST(quant_memory AS INTEGER)) AS quantity, TO_CHAR(DATE_TRUNC('day', data_insercao), 'DD-MM-YYYY') AS day, SUM(CAST(quant_memory AS INTEGER)) FILTER (WHERE type_memory = 'sodimm') AS sodimm, SUM(CAST(quant_memory AS INTEGER)) FILTER (WHERE type_memory = 'udimm') AS udimm FROM info_arq WHERE data_insercao::DATE >= (DATE_TRUNC('day', %s::DATE) - INTERVAL '6 days') AND data_insercao::DATE <= %s AND customer =  ANY(%s) AND type_memory = ANY(%s) GROUP BY DATE_TRUNC('day', data_insercao)",(dictSelect['date'],dictSelect['date'], dictSelect['costumer'], dictSelect['typeM'],))
+        result = cursor.fetchall()
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Construa uma lista de dicionários
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+        # Retorne os resultados como um objeto JSON
+        return jsonify(result_dicts)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+#Rota dos cartões/labels de processo
+@app.route('/dashboard/kpis', methods=['GET'])
+def cards_dash():
+    costumer = request.args.getlist('costumer')
+    date = request.args.get('date')
+    typeM = request.args.getlist('typeM')
+    dictSelect = {
+        'costumer': costumer,
+        'date' : date,
+        'typeM' : typeM,
+    }
+    
+    try:
+       
+        cursor = connection.cursor(cursor_factory=extras.DictCursor)
+        # EXTRACT(EPOCH FROM coluna_timestamp) = seu_valor_inteiro
+        cursor.execute("SELECT SUM(CAST(quant_memory AS INTEGER)) - (SUM(CAST(cam_erro AS INTEGER)) + SUM(CAST(mem_fail AS INTEGER))) AS labelled, SUM(CAST(bandeja AS integer)) AS trays, SUM(mem_fail) as reworks FROM info_arq WHERE data_insercao::DATE = %s AND customer =  ANY(%s) and type_memory = ANY(%s)",(dictSelect['date'],dictSelect['costumer'],dictSelect['typeM'],))
+
+        result = cursor.fetchall()
+
+        column_names = [desc[0] for desc in cursor.description]
+
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+        result = result_dicts[0]
+   
+        cursor.close()
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+#Rota ficha de erros
+@app.route('/dashboard/erros', methods=['GET'])
+def error_states():
+    typeM =[]
+    costumer = request.args.getlist('costumer')
+    date = request.args.get('date')
+    typeM = request.args.getlist('typeM')
+    dictSelect = {
+        'costumer':costumer,
+        'date' : date,
+        'typeM' : typeM,
+    }
+    try:
+     
+        cursor = connection.cursor(cursor_factory=extras.DictCursor)
+        cursor.execute("SELECT SUM(CAST(cam_erro AS INTEGER)) AS cam_erro,  SUM(CAST(mem_fail AS INTEGER)) AS mem_fail FROM info_arq WHERE data_insercao::DATE = %s AND customer =  ANY(%s) and type_memory = ANY(%s)",(dictSelect['date'],dictSelect['costumer'],dictSelect['typeM'],))
+        result = cursor.fetchall()
+
+        column_names = [desc[0] for desc in cursor.description]
+        result_dicts = [dict(zip(column_names, row)) for row in result]
+        result = result_dicts[0]
+        list_err = []
+        for key in result:
+            formatted_error={}
+            formatted_error['erro'] = f"{key} {result[key]}"
+            list_err.append(formatted_error)
+        return jsonify(list_err)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    #cur.close()
+    #connection.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ apiDashboard/putBancoData.py
@@ -0,0 +1,103 @@
+import psycopg2
+import json
+from datetime import datetime
+global url 
+url = "C:/Users/walfr/OneDrive/Ambiente de Trabalho/iLabeling_conexao/new_front_react/front-react/apiDashboard/"
+global info_arq
+info_arq = {}
+
+def read_data_js(nome_arq:str):
+        try:
+            with open(nome_arq, 'r') as arquivo_json:
+                dados_existentes = json.load(arquivo_json)
+                #print(dados_existentes)
+                return dados_existentes
+        except Exception:
+            return "No data"
+
+# Dados do dicionário info_arq
+# info_arq = {
+#     'cliente': "samsung - ELETRONICS",
+#     'bandeja': '11',
+#     'temp_total': '850.9',
+#     'min_bdj': '10min 4s',
+#     'temp_memoria': '8.5',
+#     'mem_fail': '4',
+#     'memo_index': [2, 4, 8, 9, 15, 16, 67, 68, 69],
+#     'inspec_fail': ['memoria 78', 'memoria 45', 'memoria 33'],
+#     'cam_erro': '9',
+#     "pos_rfg": ['pos 5 - fail', 'pos 9 - fail', 'pos 1 - nada', 'pos 44 - vazio'],
+#     'data_insercao': " "
+# }
+
+def send_bd(info_arq: dict):
+    try:
+        info_arq = read_data_js(url+"endProcess.json")
+        print(type(info_arq))
+        print(info_arq)
+        # Conexão com o banco de dados PostgreSQL
+        conn = psycopg2.connect(
+            database="bd_ilabeling",
+            user="postgres",
+            password="2023",
+            host="localhost",  # ou o host do seu banco de dados
+            port="5432"  # a porta padrão do PostgreSQL é 5432
+        )
+
+        # Cria um cursor para executar consultas SQL
+        cursor = conn.cursor()
+
+        # Monta a consulta SQL para inserir os dados do dicionário na tabela "info_arq"
+        consulta = """
+            INSERT INTO info_arq (
+                customer,
+                bandeja,
+                temp_total,
+                min_bdj,
+                temp_memoria,
+                mem_fail,
+                memo_index,
+                inspec_fail,
+                cam_erro,
+                pos_rfg,
+                data_insercao
+            ) VALUES (
+                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            )
+        """
+
+        # Adiciona a data de inserção ao dicionário
+        info_arq['data_insercao'] = datetime.now()
+
+        # Converte listas em formato JSON usando json.dumps
+        info_arq['memo_index'] = json.dumps(info_arq['memo_index'])
+        info_arq['inspec_fail'] = json.dumps(info_arq['inspec_fail'])
+        info_arq['pos_rfg'] = json.dumps(info_arq['pos_rfg'])
+
+        # Executa a consulta SQL
+        cursor.execute(consulta, (
+            info_arq['customer'],
+            info_arq['bandeja'],
+            info_arq['temp_total'],
+            info_arq['min_bdj'],
+            info_arq['temp_memoria'],
+            info_arq['mem_fail'],
+            info_arq['memo_index'],
+            info_arq['inspec_fail'],
+            info_arq['cam_erro'],
+            info_arq['pos_rfg'],
+            info_arq['data_insercao']
+        ))
+
+        # Confirma a transação
+        conn.commit()
+
+        print("Dados inseridos com sucesso!")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+    finally:
+        # Fecha o cursor e a conexão com o banco de dados
+        cursor.close()
+        conn.close()
