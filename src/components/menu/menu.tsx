4d903053d1bbf183d65ff70219f0fb8f027a85c8
Geanne1999 <gtfo.eng17@uea.edu.br>
2023-11-10 14:12:40 -0400

    refactor(front-react): refactor on top navigation menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router-dom";
 import "./style.css";
+import { useEffect, useState } from "react";
 export default function Menu() {
+  const [activeItem, setActiveItem] = useState(""); // State to track active item
+
+  useEffect(() => {
+    const pathname = location.pathname.substr(1);
+    setActiveItem(pathname);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    console.log("active item", activeItem);
+  }, [activeItem]);
+
   return (
     <header className="container_menu">
       <div className="container_icon">
-        <img src="/src/assets/Logo2.png" width="180" className="soft_icon" />
+        <img src="/src/assets/Logo2.png" width="320" className="soft_icon" />
       </div>
-      <div className="container_nav_menu">
-        <nav className="content_nav_menu">
-          <Link className="links_menu" to={"/"}>
-            Setup
-          </Link>
-          <Link className="links_menu" to={"/dashboard"}>
-            Dashboard
-          </Link>
-        </nav>
+      <div className="navbar">
+        <Link
+          className={`nav-item ${activeItem === "" ? "active" : ""}`}
+          to={"/"}
+        >
+          Setup
+        </Link>
+        <Link
+          to={"/dashboard"}
+          className={`nav-item ${activeItem === "dashboard" ? "active" : ""}`}
+        >
+          Dashboard
+        </Link>
       </div>
     </header>
   );
